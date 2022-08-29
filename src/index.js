9470a9cda0143e12c263677d1eb620d616a57646
tonyPin <43199056+tonyPin@users.noreply.github.com>
2022-08-29 20:33:12 +0100

    route fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import { BrowserRouter } from "react-router-dom"
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="multipage">
+    <BrowserRouter
+      basename="/
+    multipage"
+    >
       <App />
     </BrowserRouter>
   </React.StrictMode>
